93c432c9a7a75e9774b7d38d9606305928f1f485
Jose Urena <urena.jose@gmail.om>
2021-01-06 01:11:24 -0500

    main .py and files for app

---
+++ scr/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from fuzzywuzzy import process
+import numpy as np
+import pandas as pd
+import pickle
+from sqlalchemy import create_engine
+from .models import DB, Tracks
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reco_tracks.db'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    DB.init_app(app)
+
+
+    spotify = pd.read_csv('scr/spotify_tracks.csv')
+
+    spotify_matrix = np.loadtxt("scr/spotify_matrix.csv", delimiter=",")
+    # spotify numpy.array with standardize data
+
+    filename = 'scr/spotify_track_neighbors.urenaj'
+    model_knn = pickle.load(open(filename, 'rb'))
+    # pickle spotify model NearestNeighbors
+
+    @app.route("/hello")
+    def hello():
+            return "hello world"
+
+    @app.route("/", methods=["GET","POST"])
+    def song_recomender():
+        if request.method == "POST":
+            DB.create_all()
+            print(request.form)
+            name_song = request.form.get("ssong")
+            id_track = process.extractOne(name_song, spotify['name'])[2]
+            print('Song selected', spotify['name'][id_track], 'id:', id_track)
+            print('searching for recomendation....')
+            # extracting the index numbers (rows) from the model
+            distances, ids = model_knn.kneighbors(spotify_matrix[[id_track]], 
+                                                n_neighbors=3)
+            # extracting the list of the matrix, and creating a list of list.
+            spotify_ids = list(ids[0])
+            # creating a df with index return and desire columns
+            recomendation = spotify[['artists', 'name', 'id']].loc[spotify_ids]
+            recomendation.to_sql('tracks', con=DB.engine, if_exists="replace", index=False)
+            return redirect('/TracksReco')
+        else:
+            return render_template('form.html')
+
+    @app.route("/TracksReco")
+    def TracksReco():
+        tracks = Tracks.query.all()
+        return render_template('reco.html', tracks=tracks)
+
+    @app.route('/refresh')
+    def reset():
+        DB.drop_all()
+        return render_template('refresh.html', title='reset')
+
+    return app
